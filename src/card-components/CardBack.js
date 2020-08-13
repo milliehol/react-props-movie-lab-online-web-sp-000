6cbacfaf46867d6b2284d6ef4efc6097cd9a2044
Carla Holcomb <carla.programmer@gmail.com>
2020-08-13 19:58:22 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import zero from '../assets/stars/0-stars.png'
-import one from '../assets/stars/1-stars.png'
-import two from '../assets/stars/2-stars.png'
-import three from '../assets/stars/3-stars.png'
-import four from '../assets/stars/4-stars.png'
-import five from '../assets/stars/5-stars.png'
+import zero from '../assets/stars/0-stars.png';
+import one from '../assets/stars/1-stars.png';
+import two from '../assets/stars/2-stars.png';
+import three from '../assets/stars/3-stars.png';
+import four from '../assets/stars/4-stars.png';
+import five from '../assets/stars/5-stars.png';
 
 const imgMapper = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five };
 
